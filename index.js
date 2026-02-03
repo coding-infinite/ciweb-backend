aca3229974699052755a7bdb0b306ce425d9a816
shiva suryavanshi <130331021+coding-infinite@users.noreply.github.com>
2026-02-04 00:09:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("CIWEB backend running");
+});
+
+server.listen(10000, () => {
+  console.log("Server running on port 10000");
+});
